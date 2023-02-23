0c9fcb54cb2ff1629a8aabdc86f5f514f8b518c2
Eliot Gronstal <eliot.lauren@gmail.com>
2023-02-23 14:58:50 -0800

    WIP: template built but throwing using directive error. Functionality exists regardless.

--- ProjectName.Tests/ModelTests/ClassNameTests.cs
+++ ProjectName.Tests/ModelTests/ClassNameTests.cs
@@ -1,46 +1,22 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ProjectName;
+using ProjectName.Models;
+using System.Collections.Generic; 
+using System;
 
 namespace ProjectName.Tests
 {
   [TestClass]
   public class ClassNameTests
   {
-//     [TestMethod]
-// public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
-// {
-//   // any necessary logic to prep for test; instantiating new classes, etc.
-//   Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
-  //}
-    // [TestMethod]
-    // public void ItemConstructor_CreatesInstanceOfItem_Item()
-    // {
-    //   Item newItem = new Item();
-    //   Assert.AreEqual(typeof(Item), newItem.GetType());
-    // }
-
-//     [TestMethod]
-//     public void GetDescription_ReturnsDescription_String()
-//     {
-//       string description = "Walk the dog.";
-//       Item newItem = new Item(description);
-//       string result = newItem.Description;
-//       Assert.AreEqual(description, result);
-//     }
-
-//     [TestMethod]
-// public void GetDescription_ReturnsDescription_String()
-// {
-//   //Arrange
-//   string description = "Walk the dog.";
-//   Item newItem = new Item(description);
-
-//   //Act
-//   string result = newItem.Description;
-
-//   //Assert
-//   Assert.AreEqual(description, result);
-// }
-
+  [TestMethod]
+    public void ClassNameConstructor_CreatesInstanceOfClassName_ClassName()
+    {
+      // Arrange
+      ClassName newClass = new ClassName(2, 3, 8);
+      // Act
+      //not needed here
+      // Assert
+    Assert.AreEqual(typeof(ClassName), newClass.GetType());
+    }
   }
 }
--- ProjectName/Models/ClassName.cs
+++ ProjectName/Models/ClassName.cs
@@ -1,9 +1,27 @@
 using System.Collections.Generic;
+using System;
 
 namespace ProjectName.Models
 {
   public class ClassName
   {
+    private int _side1;
+    public int Side1
+    {
+      get { return _side1; }
+      set { _side1 = value; }
+    }
+    public int Side2 { get; set; }
+    private int _side3;
+    private static List<ClassName> _instances = new List<ClassName> {};
+
+    public ClassName(int length1, int length2, int length3)
+    {
+      _side1 = length1;
+      Side2 = length2;
+      _side3 = length3;
+      _instances.Add(this);
+    }
 
   }
 }
