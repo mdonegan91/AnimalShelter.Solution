e626f2916072eb4006c23454dc51d81457b6c807
Eliot Gronstal <eliot.lauren@gmail.com>
2023-02-23 14:13:10 -0800

    initial template build.

---
+++ ProjectName.Tests/ModelTests/ClassNameTests.cs
@@ -0,0 +1,46 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectName;
+
+namespace ProjectName.Tests
+{
+  [TestClass]
+  public class ClassNameTests
+  {
+//     [TestMethod]
+// public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+// {
+//   // any necessary logic to prep for test; instantiating new classes, etc.
+//   Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+  //}
+    // [TestMethod]
+    // public void ItemConstructor_CreatesInstanceOfItem_Item()
+    // {
+    //   Item newItem = new Item();
+    //   Assert.AreEqual(typeof(Item), newItem.GetType());
+    // }
+
+//     [TestMethod]
+//     public void GetDescription_ReturnsDescription_String()
+//     {
+//       string description = "Walk the dog.";
+//       Item newItem = new Item(description);
+//       string result = newItem.Description;
+//       Assert.AreEqual(description, result);
+//     }
+
+//     [TestMethod]
+// public void GetDescription_ReturnsDescription_String()
+// {
+//   //Arrange
+//   string description = "Walk the dog.";
+//   Item newItem = new Item(description);
+
+//   //Act
+//   string result = newItem.Description;
+
+//   //Assert
+//   Assert.AreEqual(description, result);
+// }
+
+  }
+}
---
+++ README.md
@@ -0,0 +1,37 @@
+# _{Application Name}_
+
+#### By Eliot Gronstal
+
+#### _{Brief description of application}_
+
+## Technologies Used
+
+* _C#_
+* _.NET_
+* _you used in your project_
+* _here_
+
+## Description
+
+_{This is a detailed description of your application. Give as much detail as needed to explain what the application does as well as any other information you want users or other developers to have.}_
+
+## Setup/Installation Requirements
+
+* _Clone this repo._
+* _Open your terminal (e.g., Terminal or GitBash) and navigate to this project's production directory called "ProjectName"._
+* _In the command line, run the command ``dotnet run`` to compile and execute the console application. Since this is a console application, you'll interact with it through text commands in your terminal._
+* _Optionally, you can run ``dotnet build`` to compile this console app without running it._
+* _cont'd_
+
+
+## Known Bugs
+
+* _Any known issues_
+
+* _Please reach out with any questions or concerns to [eliot.lauren@gmail.com]_
+
+## License
+
+_[MIT](https://opensource.org/license/mit/)_
+
+Copyright (c) _2023_ _Eliot Gronstal_
